f5bfc67c5b8d6af1cd63e8ba5c8bdf965087dd92
Tresdon Jones <tjones@spotxchange.com>
2016-10-29 20:31:07 -0600

    Large page refactor.
    1. Move to purple color scheme
    2. Remove lastfm collage
    3. add sudoku solver project
    4. Move to bootstrap
    5. Use .svg files for the icons.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
-$(document).foundation()
+
+function preload(arrayOfImages) {
+    $(arrayOfImages).each(function(){
+        $('<img/>')[0].src = this;
+    });
+}
+
+function setYear() {
+    var d = new Date();
+    $("#copyright-year").html(d.getFullYear());
+}
+
+$(function () {
+    
+    preload([
+        'assets/Github-color.svg',
+        'assets/Linkedin-color.svg',
+        'assets/stackoverflow-color.svg',
+        'assets/Spotify-color.svg',
+        'assets/Twitter-color.svg'
+    ]);
+
+
+    setYear();
+
+
+
+});
